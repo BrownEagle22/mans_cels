5a0713b01ea1ecef78134745452849535ae3a683
Ēriks Žeibe <Eriks_Zeibe@hotmail.com>
2021-06-08 01:22:54 +0300

    start page mobile

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function openMenu() {
+    document.getElementById("menu-mobile").style.display = "block";
+}
+
+function closeMenu() {
+    document.getElementById("menu-mobile").style.display = "none";
+}
